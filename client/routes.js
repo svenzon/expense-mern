5b31c1edf61fe232bf456491198c52fd841473c4
Anders Svensson <anders.m.sv@gmail.com>
2018-02-20 14:13:52 +0100

    Routes

---
+++ client/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import App from "./components/App";
+
+export const Routes = () => {
+    <Switch>
+        <Route exac path="/" component={App} />
+    </Switch>
+};
+
+export default Routes;
