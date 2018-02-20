d804368458441f6eca65023103eee5a367c07d6f
Anders Svensson <anders.m.sv@gmail.com>
2018-02-20 13:10:16 +0100

    Router and server code expanded

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var router = require("../routes/routes.js");
 var path = require("path");
-
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../client"));
 app.use(express.static(path.join(__dirname, "../client")));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: false}));
 
 app.use("/", router);
 
